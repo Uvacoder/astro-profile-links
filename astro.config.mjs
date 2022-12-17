6f4eb0068e774d995a0ff2b99f5c8411f63c32d9
Rodolfo Costa <rodolfocostaclemente@gmail.com>
2022-12-17 17:45:26 -0300

    feat: now pnpm and bundable, i hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import react from "@astrojs/react";
 // import { VitePWA } from 'vite-plugin-pwa'
 import AstroPWA from '@vite-pwa/astro';
 import critters from "astro-critters";
+import purgecss from 'astro-purgecss';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,12 +25,14 @@ export default defineConfig({
       debug: process.env.NODE_ENV !== 'production',
       forward: ["dataLayer.push"]
     }
-  }), prefetch(), react(),
+  }), prefetch({
+    selector: "a[href^='/products']"
+  }), react(),
   // astroImageTools,
   AstroPWA({
     registerType: 'autoUpdate',
     injectRegister: 'auto'
-  }), critters()],
+  }), critters(), purgecss()],
   output: "server",
   adapter: cloudflare()
 });
